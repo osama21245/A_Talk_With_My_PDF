90b81fb0489ea988ad63f4db974584e3dd422b1b
osama gamil <108416526+osama21245@users.noreply.github.com>
2025-02-16 16:23:16 +0200

    fix 10

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,37 +1,36 @@
 import { db } from "@/lib/db";
 import { userSubscriptions } from "@/lib/db/schema";
-import { stripe } from "@/lib/stribe";
+import { stripe } from "@/lib/stripe";
 import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+
 export async function POST(req: Request) {
   const body = await req.text();
-  const signature = (await headers()).get("Stripe-Signature") as string;
+  const headersList = await headers();
+  const signature = headersList.get("Stripe-Signature") as string;
   let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(
       body,
       signature,
-      process.env.STRIPE_WEBHOOK_SIGNING_SECRET as string
+      process.env.STRIPE_WEBHOOK_SIGNING_SECRET!
     );
-  } catch (error) {
-    return new NextResponse("webhook error", { status: 400 });
+  } catch {
+    return new Response("webhook error", { status: 400 });
   }
 
   const session = event.data.object as Stripe.Checkout.Session;
 
-  // new subscription created
   if (event.type === "checkout.session.completed") {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
-    if (!session?.metadata?.userId) {
-      return new NextResponse("no userid", { status: 400 });
-    }
+    
     await db.insert(userSubscriptions).values({
-      userId: session.metadata.userId,
+      userId: session.metadata?.userId!,
       stripeSubscriptionId: subscription.id,
       stripeCustomerId: subscription.customer as string,
       stripePriceId: subscription.items.data[0].price.id,
@@ -43,16 +42,15 @@ export async function POST(req: Request) {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
+
     await db
       .update(userSubscriptions)
       .set({
         stripePriceId: subscription.items.data[0].price.id,
-        stripeCurrentPeriodEnd: new Date(
-          subscription.current_period_end * 1000
-        ),
+        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
       })
       .where(eq(userSubscriptions.stripeSubscriptionId, subscription.id));
   }
 
-  return new NextResponse(null, { status: 200 });
+  return new Response(null, { status: 200 });
 }
