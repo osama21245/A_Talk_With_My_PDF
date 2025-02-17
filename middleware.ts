87f9caeb1c48ca739ba943efa83dab92caf8a86a
osama gamil <108416526+osama21245@users.noreply.github.com>
2025-02-17 05:45:31 +0200

    add create-caht-for-mob

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)',"/", "/sign-up(.*)", "/api/stripe", "/api/webhook", "/api/upload-and-create-chat", "/api/create-chat"])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)',"/", "/sign-up(.*)", "/api/stripe", "/api/webhook", "/api/upload-and-create-chat","/api/create-chat-for-mobile"])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
